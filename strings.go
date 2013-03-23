43b92d24c0eb1d6357e21b448e62c3ef13ca7838
Sergey Cherepanov <cheggaaa@gmail.com>
2013-03-23 17:45:05 +0400

    Setex

--- strings.go
+++ strings.go
@@ -42,6 +42,23 @@ func (r *Redis) Set(key string, value []byte) (err error) {
 	return
 }
 
+// Set the value and expiration of a key
+func (r *Redis) SetEx(key string, seconds int, value []byte) (err error) {
+	resp := &ReaderBase{}
+	cmd := &Command{
+		[]byte("SETEX"),
+	}
+	cmd.AddInt(seconds).Add(value)
+	err = r.Execute(cmd, resp)
+	if err != nil {
+		return
+	}
+	if ! resp.isOk() {
+		err = ErrNotOk
+	}
+	return
+}
+
 // Get the value of a key
 func (r *Redis) Get(key string) (bulk Bulk, err error) {
 	resp := &ReaderBase{}
